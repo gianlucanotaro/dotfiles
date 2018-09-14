3f430eec115a9235a468d8c7ed3dc9b8623bd2db
d <gianluca.notaro@students.fhnw.ch>
2018-09-14 21:42:40 +0200

    updated files

--- scripts/lowpower.sh
+++ scripts/lowpower.sh
@@ -3,7 +3,7 @@ while true
 do
 	sleep 5
 	battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
-	if [ $battery_level -le 30 ]
+	if [ $battery_level -le 20 ]
 	then
     	notify-send "Battery low" "Battery level is ${battery_level}%!"
 	fi
